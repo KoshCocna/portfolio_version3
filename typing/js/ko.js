fec7846d8456b37edcbca84f8c5a65d626a3a559
KoshCocna <kostsiakimstar0109@gmail.com>
2023-08-29 18:59:53 +1000

    commit 4

--- typing/js/ko.js
+++ typing/js/ko.js
@@ -1,84 +1,150 @@
-const wordInput = document.querySelector('#word-input');
-const currentWord = document.querySelector('#current-word');
-const scoreDisplay = document.querySelector('#score');
-const timeDisplay = document.querySelector('#time');
-const message = document.querySelector('#message');
-const seconds = document.querySelector('#seconds');
+const wordInput = document.querySelector("#word-input");
+const currentWord = document.querySelector("#current-word");
+const scoreDisplay = document.querySelector("#score");
+const timeDisplay = document.querySelector("#time");
+const message = document.querySelector("#message");
+const seconds = document.querySelector("#seconds");
 var level;
-let time ;
+let time;
 let score;
 let isPlaying;
 let counter;
 
 const words = [
-    "너무너무", "즐겁게","문제", "바로", "이제", "인사이더", "나오는", "단어","익숙","단어장"
-    ,"인생", "즐거움","외로움","기쁨", "슬픔", "괴로움", "사랑", "커피", "누나", "차잔", "교과서"
-    ,"바람","종이","가격","경제", "전등", "전기","물병", "민들레","연필", "변수","잠수함","전화"
-    ,"마스터", "재량","업데이트","오락","불쾌감","심의","문법","시험","문의사항","요청사항","주치의사"
+  "너무",
+  "즐겁게",
+  "문제",
+  "바로",
+  "이제",
+  "인사이더",
+  "나오는",
+  "단어",
+  "익숙",
+  "단어장",
+  "인생",
+  "즐거움",
+  "외로움",
+  "기쁨",
+  "슬픔",
+  "괴로움",
+  "사랑",
+  "커피",
+  "누나",
+  "차잔",
+  "교과서",
+  "바람",
+  "종이",
+  "가격",
+  "경제",
+  "전등",
+  "전기",
+  "물병",
+  "민들레",
+  "연필",
+  "변수",
+  "잠수함",
+  "전화",
+  "마스터",
+  "재량",
+  "업데이트",
+  "게임",
+  "불쾌감",
+  "심의",
+  "문법",
+  "시험",
+  "문의사항",
+  "요청사항",
+  "주치의사",
+  "공부",
+  "슈독",
+  "소설",
+  "팩트",
+  "이별",
+  "사랑",
+  "거짓",
+  "진실",
+  "인생",
+  "차별",
+  "흑인",
+  "검은색",
+  "흰색",
+  "백인",
+  "인종",
+  "문제",
+  "사회",
+  "복지",
+  "사이트",
+  "콜라",
+  "피자",
+  "핸드폰",
+  "비번",
+  "변경",
+  "서류",
+  "주민",
+  "등록증",
+  "아이디",
+  "개그맨",
 ];
- 
+
 //Initialize game
-function init(){
-    score = 0;
-    scoreDisplay.innerHTML = score;
-    window.clearInterval(counter);
-    level = document.getElementById("level").value;
+function init() {
+  score = 0;
+  scoreDisplay.innerHTML = score;
+  window.clearInterval(counter);
+  level = document.getElementById("level").value;
+  time = level;
+  seconds.innerHTML = level - 1;
+  showWord(words);
+  wordInput.addEventListener("input", startMatch);
+  counter = setInterval(countdown, 1000);
+  setInterval(checkStatus, 15);
+}
+
+function showWord(words) {
+  const randIndex = Math.floor(Math.random() * words.length);
+  currentWord.innerHTML = words[randIndex];
+}
+
+function countdown() {
+  if (time > 0) {
+    time--;
+  } else if (time === 0) {
+    isPlaying = false;
+  }
+  timeDisplay.innerHTML = time;
+}
+
+function checkStatus() {
+  if (!isPlaying && time === 0) {
+    message.innerHTML = "게임종료!!!";
+    score = -1;
+  }
+}
+
+function startMatch() {
+  if (matchWords()) {
+    isPlaying = true;
     time = level;
-    seconds.innerHTML = level-1;
     showWord(words);
-    wordInput.addEventListener('input', startMatch);
-    counter = setInterval(countdown, 1000);
-    setInterval(checkStatus, 15);
+    wordInput.value = "";
+    score++;
+  }
+  if (score === -1) {
+    scoreDisplay.innerHTML = 0;
+  } else {
+    scoreDisplay.innerHTML = score;
+  }
+}
+function matchWords() {
+  if (wordInput.value === currentWord.innerHTML) {
+    message.innerHTML = "성공!!!";
+    return true;
+  } else {
+    message.innerHTML = "";
+    return false;
+  }
 }
 
-function showWord(words){
-    const randIndex = Math.floor(Math.random()*words.length);
-    currentWord.innerHTML = words[randIndex];   
-}
-
-function countdown(){
-    if(time > 0){
-        time--;
-    }
-    else if(time === 0){
-        isPlaying = false;
-    }
-    timeDisplay.innerHTML = time;
-}
-
-function checkStatus(){
-    if(!isPlaying && time === 0){
-        message.innerHTML = '게임종료!!!'
-        score = -1; 
-    }
-}
-
-function startMatch(){
-    if(matchWords()){
-        isPlaying=true;
-        time = level;
-        showWord(words);
-        wordInput.value = '';
-        score++;
-    }
-    if(score === -1){
-        scoreDisplay.innerHTML = 0;
-    }
-    else{
-        scoreDisplay.innerHTML = score;
-    }
-}
-function matchWords(){
-    if(wordInput.value === currentWord.innerHTML){
-        message.innerHTML = '성공!!!';
-        return true;
-    }
-    else{
-        message.innerHTML = '';
-        return false;
-    }
-}
-
-function gotoMain(){
-    location.replace("index.html");
+function gotoMain() {
+  location.replace("index.html");
 }
